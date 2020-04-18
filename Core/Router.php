b5327391100747b851d1640de7c86526166752e2
Semih Arslan <semihsemih@me.com>
2020-04-18 03:08:35 +0300

    route dispatching and posts controller

--- Core/Router.php
+++ Core/Router.php
@@ -49,4 +49,38 @@ class Router
         return false;
     }
 
+    public function dispatch($url)
+    {
+        if ($this->match($url)) {
+            $controller = $this->params['controller'];
+            $controller = $this->convertToStudlyCaps($controller);
+
+            if (class_exists($controller)) {
+                $controller_object = new $controller();
+
+                $action = $this->params['action'];
+                $action = $this->convertToCamelCase($action);
+
+                if (is_callable([$controller_object, $action])) {
+                    $controller_object->$action();
+                } else {
+                    echo "Method $action (in controller $controller) not found!";
+                }
+            } else {
+                echo "Controller class $controller not found";
+            }
+        } else {
+            echo "No route matched";
+        }
+    }
+
+    protected function convertToStudlyCaps($string)
+    {
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', $string)));
+    }
+
+    private function convertToCamelCase($string)
+    {
+        return lcfirst($this->convertToStudlyCaps($string));
+    }
 }
--- public/index.php
+++ public/index.php
@@ -1,26 +1,13 @@
 <?php
 
+require '../App/Controllers/Posts.php';
+
 require '../Core/Router.php';
 
 $router = new Router();
 
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
-$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
 
-echo '<pre>';
-
-echo htmlspecialchars(print_r($router->getRoutes(), true));
-
-echo '</pre>';
-
-$url = $_SERVER['QUERY_STRING'];
-
-if ($router->match($url)) {
-    echo '<pre>';
-    var_dump($router->getParams());
-    echo '</pre>';
-} else {
-    echo "No route found for URL '$url'";
-}
+$router->dispatch($_SERVER['QUERY_STRING']);
