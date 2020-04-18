4b6471cb24943d289eaca02797bd75e9884e329c
Semih Arslan <semihsemih@me.com>
2020-04-19 00:49:13 +0300

    autoload classes

---
+++ App/Controllers/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Controllers;
+
+
+class Home
+{
+    public function index()
+    {
+        echo "Hello from the index action in the Home controller!";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,10 +1,14 @@
 <?php
 
-require '../App/Controllers/Posts.php';
+spl_autoload_register(function ($class) {
+    $root = dirname(__DIR__);
+    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
+    if (is_readable($file)) {
+        require $root . '/' . str_replace('\\', '/', $class) . '.php';
+    }
+});
 
-require '../Core/Router.php';
-
-$router = new Router();
+$router = new Core\Router();
 
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('{controller}/{action}');
