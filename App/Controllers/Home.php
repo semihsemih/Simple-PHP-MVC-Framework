8e5e46cec1ab0db805754c94295f8d577eb34b1c
Semih Arslan <semihsemih@me.com>
2020-04-20 09:37:33 +0300

    call a method before and after every action in controller

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -6,7 +6,17 @@ namespace App\Controllers;
 
 class Home extends \Core\Controller
 {
-    public function index()
+    protected function before()
+    {
+        echo "(before) ";
+    }
+
+    protected function after()
+    {
+        echo " (after)";
+    }
+
+    public function indexAction()
     {
         echo "Hello from the index action in the Home controller!";
     }
--- Core/Controller.php
+++ Core/Controller.php
@@ -12,4 +12,26 @@ abstract class Controller
     {
         $this->route_params = $route_params;
     }
+
+    public function __call($name, $arguments)
+    {
+        $method = $name . 'Action';
+
+        if (method_exists($this, $method)) {
+            if ($this->before() !== false) {
+                call_user_func([$this, $method], $arguments);
+                $this->after();
+            }
+        } else {
+            echo "Method $method not found in controller " . get_class($this);
+        }
+    }
+
+    protected function before()
+    {
+    }
+
+    protected function after()
+    {
+    }
 }
