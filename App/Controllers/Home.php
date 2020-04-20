63ec80b902a2e6b0ebbc395707e665422df1e511
Semih Arslan <semihsemih@me.com>
2020-04-20 15:02:28 +0300

    rendering views

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -3,21 +3,21 @@
 
 namespace App\Controllers;
 
+use \Core\View;
 
 class Home extends \Core\Controller
 {
     protected function before()
     {
-        echo "(before) ";
     }
 
     protected function after()
     {
-        echo " (after)";
     }
 
     public function indexAction()
     {
-        echo "Hello from the index action in the Home controller!";
+        // echo "Hello from the index action in the Home controller!";
+        View::render('Home/index.php');
     }
 }
